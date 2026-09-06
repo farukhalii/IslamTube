34180f57a4d686264870d05e4865859eed5dbf58
farukhalii <farukhalidesigner@gmail.com>
2026-09-06 20:11:32 +0500

    Change MainActivity to extend AppCompatActivity

--- app/src/main/java/com/example/islamtube/MainActivity.kt
+++ app/src/main/java/com/example/islamtube/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.islamtube
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
-
